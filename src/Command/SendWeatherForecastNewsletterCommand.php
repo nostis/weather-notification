72df9f3c883bd33d461242748c8c32266a7d2eae
nostis <norbert_2000@o2.pl>
2020-09-02 19:26:02 +0200

    Add command stub for sending weather forecast newsletter

---
+++ src/Command/SendWeatherForecastNewsletterCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class SendWeatherForecastNewsletterCommand extends Command
+{
+    protected static $defaultName = 'app:send:forecast:newsletter';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Send weather forecast newsletter to customers')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+        return Command::SUCCESS;
+    }
+}
