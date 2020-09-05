bc3469ee7da72b4fb8771d25d161b65003a2577c
nostis <norbert_2000@o2.pl>
2020-09-05 14:07:33 +0200

    Private comment

--- src/Command/SendWeatherForecastNewsletterCommand.php
+++ src/Command/SendWeatherForecastNewsletterCommand.php
@@ -7,7 +7,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-class SendWeatherForecastNewsletterCommand extends Command
+class SendWeatherForecastNewsletterCommand extends Command //@todo
 {
     protected static $defaultName = 'app:send:forecast:newsletter';
 
